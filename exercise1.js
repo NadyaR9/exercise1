98460c349b1f16b68e94b296abb81f6883e257eb
Nadya <snotra.notra@gmail.com>
2020-03-28 00:42:51 +0300

    читай задания внимательнее^^

--- exercise1.js
+++ exercise1.js
@@ -1,20 +1,20 @@
 'use strict';
-let money="Ваш бюджет на месяц?";
-let time= "Введите дату в формате YYYY-MM-DD";
+let money=prompt("Ваш бюджет на месяц?", "");
+let time= prompt("Введите дату в формате YYYY-MM-DD","");
 let appData={
+   budget:money,
+   timeData:time,
    expenses:{} ,
-   optionalExpenses: "",
+   optionalExpenses: {},
    income:[],
    savings:false,
 };
-appData.budget=money;
-appData.timeData=time;
 
 let question1=prompt("Введите обязательную статью расходов в этом месяце","bills");
 let question2=prompt("Во сколько обойдется?","100");
 appData.expenses[question1]=question2;
 console.log(appData.expenses);
-alert(`Ваш бюджет на один день составляет ${Math.floor(appData.expenses[question1]/30)}`);
+alert(`Ваш бюджет на один день составляет ${Math.floor(appData.budget/30)}`);
 //Сколько типов данных существует в JS?-7
 //Как вывести информацию в консоль?-console.log()
 //Какая функция операторов || и &&?-логическое or и and
