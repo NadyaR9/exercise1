0d8910e6807d395e992e54b8e545fac911c5af49
Nadya <snotra.notra@gmail.com>
2020-03-28 16:08:42 +0300

    lesson 3, work with function

--- exercise1.js
+++ exercise1.js
@@ -1,70 +1,91 @@
 'use strict';
-let money=+prompt("Ваш бюджет на месяц?", "1200");
-let time= prompt("Введите дату в формате YYYY-MM-DD","");
+let money,time;
+start();
+function start(){   
+   time= prompt("Введите дату в формате YYYY-MM-DD","");
+   money=+prompt("Ваш бюджет на месяц?", "12000");
+   while (isNaN(money) || money == "" || money == null) {
+      money=+prompt("Ваш бюджет на месяц?", "12000");
+   } 
+}
 let appData={
    budget:money,
    timeData:time,
    expenses:{} ,
    optionalExpenses: {},
    income:[],
-   savings:false,
+   savings:true,
 };
 
-for (let i=0; i<2; i++){
-   let question1=prompt("Введите обязательную статью расходов в этом месяце","bills");
-   let question2=prompt("Во сколько обойдется?","100");   
-if (typeof(question1)==='string' && typeof(question2)==='string' && typeof(question1)!=null && typeof(question2)!=null &&
- question1!='' && question2!='' && question1.length<50)
- {
-   appData.expenses[question1]=question2;
- } else {
-   i--;
- }
+function chooseExpenes(){
+   let i=0;
+   do {
+      let question1=prompt("Введите обязательную статью расходов в этом месяце","bills");
+      let question2=prompt("Во сколько обойдется?","100");   
+   if (typeof(question1)==='string' && typeof(question2)==='string' && typeof(question1)!=null && 
+   typeof(question2)!=null &&   question1!='' && question2!='' && question1.length<50)
+    {
+      appData.expenses[question1]=question2;
+      i++;
+    }
+   } while ( i<2 );
 }
-/*
-let i=0;
-while (i<2)
-{
-   let question1=prompt("Введите обязательную статью расходов в этом месяце","bills");
-   let question2=prompt("Во сколько обойдется?","100");   
-if (typeof(question1)==='string' && typeof(question2)==='string' && typeof(question1)!=null && typeof(question2)!=null &&
- question1!='' && question2!='' && question1.length<50)
- {
-   appData.expenses[question1]=question2;
-   i++;
- }
-}
-do {
-   let question1=prompt("Введите обязательную статью расходов в этом месяце","bills");
-   let question2=prompt("Во сколько обойдется?","100");   
-if (typeof(question1)==='string' && typeof(question2)==='string' && typeof(question1)!=null && typeof(question2)!=null &&
- question1!='' && question2!='' && question1.length<50)
- {
-   appData.expenses[question1]=question2;
-   i++;
- }
-} while ( i<2 );
-*/
-console.log(appData.expenses);
-alert(`Ваш бюджет на один день составляет ${Math.floor(appData.budget/30)}`);
 
-if ( appData.budget <= 1000 ) 
-{
-   alert('Достаток ниже среднего');
+
+function detectDayBudget(){
+
+   alert(`Ваш бюджет на один день составляет ${(appData.budget/30).toFixed(1)}`);
+
 }
-else if ( appData.budget <= 2500 && appData.budget > 1000 )  
-{
-   alert("Достаток средний");
+
+function detectLevel(){
+
+   if ( appData.budget <= 1000 ) 
+   {
+      alert('Достаток ниже среднего');
+   }
+   else if ( appData.budget <= 2500 && appData.budget > 1000 )  
+   {
+      alert("Достаток средний");
+   }
+   else if (appData.budget>2500) 
+   {
+      alert("Достаток выше среднего");
+   }
+   else 
+   { 
+      alert("Наша статистика не располагает такими данными"); 
+   }
+
 }
-else if (appData.budget>2500) 
-{
-   alert("Достаток выше среднего");
+function chooseOptExpensens(){
+
+   let i=0;
+   do{
+      let answer=prompt("Статья необязательных расходов?","sweet");
+      if (answer != '' || answer != null )
+      {
+         appData.optionalExpenses[i]=answer;
+         i++;
+      }
+      
+   }while (i<3);
+
 }
-else 
-{ 
-   alert("Наша статистика не располагает такими данными"); 
+
+function countSavings(){
+
+   if (appData.savings){
+
+      let sum=+prompt("Какова сумма на вашем счете?","1000"),
+      percent=prompt("Под какой процент?","12%");
+      alert(`Ежемесячный доход с вашего вклада составляет ${sum/100/12*parseInt(percent)}`);
+
+   }
 }
-//Сколько типов данных существует в JS?-7
-//Как вывести информацию в консоль?-console.log()
-//Какая функция операторов || и &&?-логическое or и and
+chooseExpenes();
+detectDayBudget();
+detectLevel();
+chooseOptExpensens();
+countSavings();
 
